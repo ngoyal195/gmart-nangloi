d9541b509ea07ea6620a5533dbc2fa497ceab26a
ngoyal195 <namangoyal195@gmail.com>
2025-08-16 13:53:11 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'docs',
-  },
-})
+  plugins: [react()],
+  base: "/gmart-nangloi/", // 👈 your repo name here
+});
