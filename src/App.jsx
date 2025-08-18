c7b5d17d49df90be5e3226c75f125213c3150c96
ngoyal195 <namangoyal195@gmail.com>
2025-08-18 12:47:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,7 @@ export default function App() {
       100% { transform: translateX(-50%); }
     }
     .hero-slide {
-      animation: slide-hero 4s linear infinite;
+      animation: slide-hero 30s linear infinite;
       /* pause on hover (optional) */
     }
     .hero-slide:hover {
