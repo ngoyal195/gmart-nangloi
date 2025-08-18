e5e61076d87dac9352b021bec180ac494967cc12
ngoyal195 <namangoyal195@gmail.com>
2025-08-18 12:16:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Brands from "./Brands.jsx";
+import AllProducts from "./AllProducts.jsx"; // <-- new import
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/brands" element={<Brands />} />
+        <Route path="/all-products" element={<AllProducts />} /> {/* new route */}
       </Routes>
     </HashRouter>
   </React.StrictMode>
